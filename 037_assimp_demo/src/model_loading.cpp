d9bf4c88538c25bc7750646aecda0f35084e9bde
zerg <xjw580@qq.com>
2025-03-24 18:11:27 +0800

    优化代码

--- 037_assimp_demo/src/model_loading.cpp
+++ 037_assimp_demo/src/model_loading.cpp
@@ -172,6 +172,11 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
+
+
+
+
+
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
---
+++ cmake/build.cmake
@@ -0,0 +1,29 @@
+macro (my_add_assimp_dll)
+
+    # 设置输出目录
+    set(EXECUTABLE_OUTPUT_DIR ${CMAKE_BINARY_DIR}/${PROJECT_NAME})
+
+    message(CMAKE_SOURCE_DIR:${CMAKE_SOURCE_DIR})
+    message(EXECUTABLE_OUTPUT_DIR:${EXECUTABLE_OUTPUT_DIR})
+
+    # 2. 定义 DLL 源路径和目标路径
+    set(DLL_SOURCE "${CMAKE_SOURCE_DIR}/libs/lib/assimp-vc143-mt.dll")
+    set(DLL_DEST "${EXECUTABLE_OUTPUT_DIR}/assimp-vc143-mt.dll")
+
+    # 3. 添加自定义命令，在构建后拷贝 DLL
+    add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            "${DLL_SOURCE}"
+            "${DLL_DEST}"
+            COMMENT "Copying assimp-vc143-mt.dll to executable directory: ${DLL_DEST}"
+    )
+endmacro()
+
+macro (my_add_target name type)
+    file(GLOB_RECURSE srcs CONFIGURE_DEPENDS src/*.cpp src/*.c include/*.h)
+    if ("${type}" MATCHES "EXECUTABLE")
+        add_executable(${name} ${srcs})
+    else()
+        add_library(${name} ${type} ${srcs})
+    endif()
+endmacro()
